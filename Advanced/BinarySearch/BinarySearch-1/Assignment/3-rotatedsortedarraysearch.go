db4385f6b4d820f15cc41ae755d04e0057158da8
nithinkm8055 <nithinkm.tcs@gmail.com>
2022-08-02 23:44:53 +0200

    Advanced: Binary Search - 1 Assignment(all)

--- Advanced/BinarySearch/BinarySearch-1/Assignment/3-rotatedsortedarraysearch.go
+++ Advanced/BinarySearch/BinarySearch-1/Assignment/3-rotatedsortedarraysearch.go
@@ -1,1 +1,30 @@
 package Assignment
+
+func search(A []int, B int) int {
+	l := 0
+	r := len(A) - 1
+
+	for l <= r {
+
+		mid := (l + r) / 2
+
+		if A[mid] == B {
+			return mid
+		} else if A[mid] >= A[l] {
+
+			if A[mid] > B && B >= A[l] {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		} else if A[mid] < A[l] {
+			if B > A[mid] && B <= A[r] {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		}
+	}
+
+	return -1
+}
