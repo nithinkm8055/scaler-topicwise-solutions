9d4faa62f14a466a32814c608a2de64494f382d8
nithinkm8055 <nithinkm.tcs@gmail.com>
2022-09-07 23:03:40 +0200

    Advanced: Stacks-2 (Homework all - one TLE)

---
+++ Advanced/Stacks/Stacks-2/Homework/1-perfectnumbers.go
@@ -0,0 +1,77 @@
+package Homework
+
+import "strconv"
+
+type Node struct {
+	val  int
+	next *Node
+}
+
+func newNode(val int) *Node {
+	node := new(Node)
+	node.val = val
+	node.next = nil
+	return node
+}
+
+// TLE
+func PerfectNumbers(A int) string {
+
+	counter := 0
+
+	var head *Node
+	var tail *Node
+
+	head = newNode(1)
+	tail = newNode(2)
+	head.next = tail
+
+	for head != nil {
+
+		temp := head.val
+		head = head.next // remove one element
+
+		node1 := newNode(temp*10 + 1)
+		tail.next = node1
+		tail = tail.next
+
+		node2 := newNode(temp*10 + 2)
+		tail.next = node2
+		tail = tail.next
+
+		if isPalindrome(node1.val) {
+			counter++
+			if counter == A {
+				return strconv.Itoa(node1.val)
+			}
+		}
+		if isPalindrome(node2.val) {
+			counter++
+			if counter == A {
+				return strconv.Itoa(node2.val)
+			}
+		}
+	}
+
+	return ""
+
+}
+
+func isPalindrome(n int) bool {
+
+	revNum := 0
+	temp := n
+	len := 0
+	for temp > 0 {
+		r := temp % 10
+		revNum = revNum*10 + r
+		temp = temp / 10
+		len++
+	}
+
+	if n == revNum && (len%2) == 0 {
+		return true
+	}
+
+	return false
+}
---
+++ Advanced/Stacks/Stacks-2/Homework/2-taskscheduling.go
@@ -0,0 +1,40 @@
+package Homework
+
+func TaskScheduling(A []int, B []int) int {
+
+	var head *Node
+	var tail *Node
+
+	head = newNode(A[0])
+	tail = newNode(A[1])
+
+	head.next = tail
+
+	for i := 2; i < len(A); i++ {
+		node := newNode(A[i])
+		tail.next = node
+		tail = node
+	}
+	counter := 0
+	for i := range B {
+		for head != nil {
+
+			if head.val != B[i] {
+				temp := head
+				head = head.next
+				temp.next = nil
+
+				tail.next = temp
+				tail = tail.next
+				counter++
+			} else {
+				head = head.next
+				counter++
+				break
+			}
+
+		}
+	}
+
+	return counter
+}
