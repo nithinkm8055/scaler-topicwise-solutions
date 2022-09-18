bfba31f0b530cde065e72a318bf6a98f815e6666
nithinkm8055 <nithinkm.tcs@gmail.com>
2022-09-18 19:53:46 +0200

    Advanced: Binary Search - refactoring

--- Advanced/BinarySearch/BinarySearch-2/Homework/1-kthprice.go
+++ Advanced/BinarySearch/BinarySearch-2/Homework/1-kthprice.go
@@ -1,17 +1,49 @@
 package Homework
 
-import "sort"
+func solve(A []int, k int) int {
+	start := A[0]
+	end := A[0]
 
-// TODO: do with Binary Search
-func solve(A []int, B int) int {
-	sort.Ints(A)
+	ans := -1
 
-	count := 0
 	for i := range A {
-		count++
-		if count == B {
-			return A[i]
+		if A[i] <= start {
+			start = A[i]
+		} else if A[i] > end {
+			end = A[i]
 		}
 	}
-	return -1
+
+	for start <= end {
+		mid := (start + end) / 2
+		numSmaller := 0
+
+		for i := 0; i < len(A); i++ {
+			if A[i] <= mid {
+				numSmaller++
+			}
+		}
+
+		if numSmaller >= k {
+			end = mid - 1
+			ans = mid
+		} else {
+			start = mid + 1
+		}
+	}
+
+	return ans
 }
+
+//func solve(A []int, B int) int {
+//	sort.Ints(A)
+//
+//	count := 0
+//	for i := range A {
+//		count++
+//		if count == B {
+//			return A[i]
+//		}
+//	}
+//	return -1
+//}
